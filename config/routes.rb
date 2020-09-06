7046fbe0f4b6b655cfe4b77023c53ae77aace362
hiroya418 <hiroto03@gmail.com>
2020-09-06 19:38:46 +0900

    ユーザー　コントローラー、アクション作成まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
+  #ユーザー管理、マイページ実装用（onlyで必要な機能だけ！）
+  resources :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
