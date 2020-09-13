bdb713e8a6dc7358b5489ec267a90495e4c57911
hiroya418 <hiroto03@gmail.com>
2020-09-13 16:16:08 +0900

    side-barまで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,27 @@
 class UsersController < ApplicationController
 
+  before_action :authenticate_user!, only: [:mypage]
+
   def show
     @nickname = current_user.nickname
-    @items = current_user.items
+    @user = User.where(user_id: current_user.id)
+  end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  #以下、自信ないです
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'items#index'
   #ユーザー管理、マイページ実装用（onlyで必要な機能だけ！）
   resources :users
-
+  get    'users/:id'   =>  'users#show'
+  get '/mypage' => 'users#mypage'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
