e70019f259961f66ed342532b929fafac4fa3efd
hiroya418 <hiroto03@gmail.com>
2020-09-21 19:57:10 +0900

    マークアップ05

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,12 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def logout
+  end
+
+  def creditcard
+  end
+
   def update
     if current_user.update(user_params)
       redirect_to root_path
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   #ユーザー管理、マイページ実装用（onlyで必要な機能だけ！）
   # resources :users
   get 'users/show' => 'users#show'
+  get 'users/log-out' => 'users#logout'
+  get 'users/creditcard' => 'users#creditcard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
