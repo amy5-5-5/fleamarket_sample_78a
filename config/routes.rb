94064362039e80458d3e9108796779f237c13626
t-hosakaa <64078369+t-hosakaa@users.noreply.github.com>
2020-09-26 20:24:23 +0900

    ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :items 
+  # 最終的に消す。ビュー表示のために設定中
+  root 'items#index'
+  # except: [:show]は最終的に消す。ビュー表示のために設定中
+  resources :items, except: [:index, :show]
 end
