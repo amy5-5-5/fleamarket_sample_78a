f1ae2d0f675c93c4a3e78d9a058b3bf197e452d6
hiroya418 <hiroto03@gmail.com>
2020-09-27 18:23:48 +0900

    マークアップ14

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
   root 'items#index'
-  #ユーザー管理、マイページ実装用（onlyで必要な機能だけ！）
-  resources :users
-  
+  #ユーザー管理、マイページ実装用（onlyで必要な機能だけ！
+
   get 'users/show' => 'users#show'
   get 'users/log-out' => 'users#logout'
   get 'users/creditcard' => 'users#creditcard'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
