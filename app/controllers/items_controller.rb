ab0eb0fe732617f7ad876cb72cfbd7a076438170
t-hosakaa <64078369+t-hosakaa@users.noreply.github.com>
2020-10-02 14:01:59 +0900

    出品ページ・商品詳細ページ・編集ページのビュー調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,7 @@ class ItemsController < ApplicationController
     # ログインユーザーのIDを自動保存する処理が必要（ユーザー機能ができてから）
     @item = Item.new(item_params)
     if @item.save
-      redirect_to new_item_path
+      redirect_to root_path
     else
       @item.images.new
       render :new
@@ -27,9 +27,18 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
   
   def item_params
-    params.require(:item).permit(:name, :text, :condition_id, :category_id, :burden_id, :area_id, :shipping_date_id, :price, :brand, images_attributes: [:src])
+    params.require(:item).permit(:name, :text, :condition_id, :category_id, :burden_id, :area_id, :shipping_date_id, :price, :brand, images_attributes: [:src, :_destroy, :id])
   end
 end
