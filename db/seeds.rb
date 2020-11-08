73458267edfe206ee3c587a19e1b94934fe40b7a
hiroya418 <hiroto03@gmail.com>
2020-11-08 16:41:06 +0900

    ancestry 修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require "csv"
 
-CSV.foreach('db/category.csv') do |row|
-  Category.create(:id => row[0], :name => row[1], :ancestry => row[2])
+CSV.foreach('db/category.csv',headers: true) do |row|
+  Category.create(id: row[0], name: row[1], ancestry: row[2])
 end 
